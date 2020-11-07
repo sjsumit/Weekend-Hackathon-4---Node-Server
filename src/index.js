038e58989a4b844c3d0016ab94f967a3ad20359d
sjsumit <46019235+sjsumit@users.noreply.github.com>
2020-11-07 19:23:54 +0530

    Update index.js
    chanes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 var http = require("http");
-
 const httpServer = http.createServer(handleServer);
-
-
-function handleServer(req, res) {
-  
+const obj=
+{
+    phone: '18602100000',
+    email: 'guestcaredominos@jublfood.com'
 }
-
+function handleServer(req, res) {
+    if(req.url==='/welcome'){
+        res.write("Welcome to Dominos!");
+        res.statusCode=200;
+        res.end();
+    }
+    else if(req.url==='/contact'){
+        res.write(JSON.stringify(obj));
+        res.statusCode=200;s
+        res.end();
+    }
+    else{
+        res.statusCode=404;
+        res.end();
+    }
+   
+}
+httpServer.listen(8081);
 module.exports = httpServer;
